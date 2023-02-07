2ef99766c29aa7fd63f7902e7628f1f0f2df4aef
Burak Arslan <burak.arslan@arskom.com.tr>
2023-02-07 18:29:46 +0300

    import fmt and loguru

---
+++ fmt.cmake
@@ -0,0 +1,23 @@
+﻿
+add_library(fmt STATIC
+    fmt/include
+    fmt/include/fmt
+    fmt/include/fmt/args.h
+    fmt/include/fmt/chrono.h
+    fmt/include/fmt/color.h
+    fmt/include/fmt/compile.h
+    fmt/include/fmt/core.h
+    fmt/include/fmt/format-inl.h
+    fmt/include/fmt/format.h
+    fmt/include/fmt/os.h
+    fmt/include/fmt/ostream.h
+    fmt/include/fmt/printf.h
+    fmt/include/fmt/ranges.h
+    fmt/include/fmt/std.h
+    fmt/include/fmt/xchar.h
+
+    fmt/src/format.cc
+    fmt/src/os.cc
+)
+
+target_include_directories(fmt PUBLIC fmt/include)
---
+++ loguru.cmake
@@ -0,0 +1,11 @@
+﻿
+include(fmt.cmake)
+
+add_library(loguru STATIC
+    loguru/loguru.hpp
+    loguru/loguru.cpp
+)
+
+target_compile_definitions(loguru PUBLIC LOGURU_USE_FMTLIB=1)
+target_link_libraries(loguru PUBLIC fmt)
+
